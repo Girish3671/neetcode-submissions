bbf24e31eef34189d87ee5bf3ca517816ab29b43
Girish K <girishseri03@gmail.com>
2026-08-27 21:29:45 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto it:s){
+            if((it=='(')||(it=='{')||(it=='[')){
+                st.push(it);
+            }else{
+                if(st.empty()){
+                    return false;
+                }else{
+                    char top=st.top();
+                    if(it==')'&&top!='(' || it=='}'&&top!='{'|| it==']'&& top!='[' ){
+                        return false;
+                    }
+                    st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+};
